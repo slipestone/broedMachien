0ee0c6697a39adef182e2336f94ca871ecb6f6e4
Marcel Franke <marcel.franke@anatta.be>
2017-12-23 22:02:14 +0100

    server skeleton

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+app.get('/temperature', function(req,res)
+{
+    res.send('24 °C');
+});
+
+app.get('/humidity', function(req,res) {
+    res.send('48%');
+});
+
+app.listen(3000,function(){
+    console.log('server is listening on port 3000');
+})
+
