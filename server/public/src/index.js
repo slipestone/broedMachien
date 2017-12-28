29b3f7ce5a1b402a6fab0d6611b923dafeed1f47
Marcel Franke <marcel.franke@anatta.be>
2017-12-28 22:09:30 +0100

    async call in index to service

--- server/public/src/index.js
+++ server/public/src/index.js
@@ -1,5 +1,22 @@
 $(document).ready( () =>{
-    $('#temperature').text("piep");
+
+    //get temp async
+    $.ajax({
+        url: "/temperature",
+        method: "GET",
+        headers: {
+            "accept":"application/json;odata=verbose",
+        },
+        success: function(data){            
+            $('#temperature').text(data);
+        },
+        error: function (err){
+            alert (err);
+        }
+
+    });
+
+
     $('#humidity').text("boing");
     
 })
