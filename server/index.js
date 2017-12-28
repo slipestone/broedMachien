e2a2c04cec7522c31e5be26425d29e30a330e9cc
Marcel Franke <marcel.franke@anatta.be>
2017-12-28 21:39:04 +0100

    cached synchrnonous reading DHT11

--- server/index.js
+++ server/index.js
@@ -1,27 +1,20 @@
 const express = require('express');
 const app = express();
-const sensor = require('node-dht-sensor');
+//const sensor = require('node-dht-sensor');
+//const getSensorReadings = require('./get-sensor-readings')
+const getCachedSensorReadings = require('./get-cached-sensor-readings');
+
 
 
 app.get('/temperature', function (req, res) {
-    sensor.read(11, 4, function (err, temperature,
-        humidity) {
-        if (err) {
-            console.error(err);
-        }
-        res.send(temperature.toFixed(1) + '°C');
-    });
+
+    res.send(getCachedSensorReadings.getTemperature().toFixed(1)  + '°C')    
 });
 
 app.get('/humidity', function (req, res) {
-    sensor.read(11, 4, function (err, temperature,
-        humidity) {
-        if (!err) {
-            res.send(humidity.toFixed(1) + '%');
-
-        }
-    });
+    res.send(getCachedSensorReadings.getHumidity().toFixed(1) + '%')                  
 });
+
 
 app.listen(3000, function () {
     console.log('server is listening on port 3000');
