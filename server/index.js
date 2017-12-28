3c7459e0c2eb97090a658add06dd2feee5ab4fed
Marcel Franke <marcel.franke@anatta.be>
2017-12-28 20:48:16 +0100

    updated server to read from DHT11 sensor

--- server/index.js
+++ server/index.js
@@ -1,16 +1,29 @@
 const express = require('express');
-
 const app = express();
-app.get('/temperature', function(req,res)
-{
-    res.send('24 °C');
+const sensor = require('node-dht-sensor');
+
+
+app.get('/temperature', function (req, res) {
+    sensor.read(11, 4, function (err, temperature,
+        humidity) {
+        if (err) {
+            console.error(err);
+        }
+        res.send(temperature.toFixed(1) + '°C');
+    });
 });
 
-app.get('/humidity', function(req,res) {
-    res.send('48%');
+app.get('/humidity', function (req, res) {
+    sensor.read(11, 4, function (err, temperature,
+        humidity) {
+        if (!err) {
+            res.send(humidity.toFixed(1) + '%');
+
+        }
+    });
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log('server is listening on port 3000');
 })
 
