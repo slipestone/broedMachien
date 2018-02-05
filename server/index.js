117a86b44b558773ea6c2469e3049a43d3598821
Marcel Franke <marcel.franke@anatta.be>
2018-02-05 20:31:25 +0100

    we are in business

--- server/index.js
+++ server/index.js
@@ -4,12 +4,10 @@ const express = require('express');
 const path = require('path')
 const app = express();
 const relais = require('./relais');
-//var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO 
-// 
- 
-//const sensor = require('node-dht-sensor');
-//const getSensorReadings = require('./get-sensor-readings')
+
 const getCachedSensorReadings = require('./get-cached-sensor-readings');
+
+relais.init(5); //init relais on port 5
 
 app.use('/public', express.static(path.join(__dirname,
     'public')))
@@ -23,11 +21,31 @@ app.get('/temperature', function (req, res) {
 
 app.get('/humidity', function (req, res) {
     res.json({
-        value: 
-    getCachedSensorReadings.getHumidity().toFixed(1)
+        value: getCachedSensorReadings.getHumidity().toFixed(1)
       })
 });
 
+app.get('/all', function (req, res) {
+    res.json({
+        temperature: getCachedSensorReadings.getTemperature().toFixed(1),
+        humidity: getCachedSensorReadings.getHumidity().toFixed(1)
+    })
+});
+
+
+app.get('/relaisOn', function (req, res) {
+    relais.setStatus(1); 
+    res.json({
+        relais: relais.getStatus().toFixed(0)
+    })
+});
+
+app.get('/relaisOff', function (req, res) {
+    relais.setStatus(0); 
+    res.json({
+        relais: relais.getStatus().toFixed(0)
+    })
+});
 
 
 app.listen(3000, function () {
@@ -41,16 +59,16 @@ httpServer.start(2999);*/
 
 //CONTROL LOGIC
 
-setInterval(controlTemperature,1000);
+//setInterval(controlTemperature,1000);
 
-relais.init(5)
 setInterval(controlTemperature,1000);
 
 
 function controlTemperature(){
+
     var temp = getCachedSensorReadings.getTemperature()
-    if (temp > 38) {
-        console.log(">38 relais is off ("+ temp+")" )
+    if (temp != null && temp > 38) {
+        console.log(" temp: "+ temp+")" )
         if (relais.getStatus() === 1) { //if relais is On
             relais.setStatus(0);           //then swithc it off
         }
@@ -60,7 +78,7 @@ function controlTemperature(){
     else if (temp <= 37){
         console.log("<= 37 relais is on ("+ temp+")" ) 
         if (relais.getStatus() === 0) { //if relais is On
-            relais.getStatus(1);           //then swithc it off
+            relais.setStatus(1);           //then swithc it off
         }
       
     }    
