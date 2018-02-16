13572ac13ae3225c61bec52a0a202e442b4dc4b4
Marcel Franke <marcel.franke@anatta.be>
2018-02-17 00:44:15 +0100

    added graphs

--- server/public/src/index.js
+++ server/public/src/index.js
@@ -1,7 +1,106 @@
 $(document).ready( () =>{
 
+//CHART LOGIC
+const temperatureCanvasCtx = 
+document.getElementById('temperature-chart').getContext('2d')
+
+const temperatureChartConfig = {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      data: [],
+      backgroundColor: 'rgba(255, 205, 210, 0.5)'
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          suggestedMin: 36,
+          suggestedMax: 39
+        }
+      }]
+    }
+  }
+}
+const temperatureChart = new Chart(temperatureCanvasCtx, 
+temperatureChartConfig)
+
+const humidityCanvasCtx = 
+document.getElementById('humidity-chart').getContext('2d')
+
+const humidityChartConfig = {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      data: [],
+      backgroundColor: 'rgba(197, 202, 233, 0.5)'
+   }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          suggestedMin: 20,
+          suggestedMax: 80
+        }
+      }]
+    }
+  }
+}
+const humidityChart = new Chart(humidityCanvasCtx, 
+ humidityChartConfig)
+
+const pushData = (arr, value, maxLen) => {
+  arr.push(value)
+  if (arr.length > maxLen) {
+    arr.shift()
+  }
+}
+
+const humidityDisplay = 
+document.getElementById('humidity-display')
+const temperatureDisplay =    
+document.getElementById('temperature-display')
+
+const fetchTemperature = () => {
+  fetch('/temperature')
+    .then(results => {
+      return results.json()
+    })
+    .then(data => {
+      const now = new Date()
+      const timeNow = now.getHours() + ':' + 
+ now.getMinutes() + ':' + now.getSeconds()
+      pushData(temperatureChartConfig.data.labels, 
+timeNow, 50)
+      pushData(temperatureChartConfig.data.datasets[0]
+.data, data.value, 50)
+      temperatureChart.update()
+      temperatureDisplay.innerHTML = '<strong>' + 
+data.value + '</strong>'
+    })
+}
+//END CHART LOGIC
+
+
+
+
+
     //get temp async
-    $.ajax({
+    getTempAsync = () => {$.ajax({
         url: "/temperature",
         method: "GET",
         headers: {
@@ -9,14 +108,27 @@ $(document).ready( () =>{
         },
         success: function(data){            
             $('#temperature-display').text(data.value);
+
+            //CHART
+            const now = new Date()
+            const timeNow = now.getHours() + ':' + 
+       now.getMinutes() + ':' + now.getSeconds()
+            pushData(temperatureChartConfig.data.labels, 
+      timeNow, 50)
+            pushData(temperatureChartConfig.data.datasets[0]
+      .data, data.value, 50)
+            temperatureChart.update()
+            ///CHART
+
         },
         error: function (err){
             alert (err);
         }
 
     });
+}
 
-    $.ajax({
+    getHumAsync = () => {$.ajax({
         url: "/humidity",
         method: "GET",
         headers: {
@@ -24,12 +136,30 @@ $(document).ready( () =>{
         },
         success: function(data){            
             $('#humidity-display').text(data.value);
+            // CHART
+            const now = new Date()
+          const timeNow = now.getHours() + ':' + 
+    now.getMinutes() + ':' + now.getSeconds()
+          pushData(humidityChartConfig.data.labels, timeNow,
+     10)
+          pushData(humidityChartConfig.data.datasets[0].data,
+     data.value, 10)
+          humidityChart.update()
+            // CHART
         },
         error: function (err){
             alert (err);
         }
+    
 
     });
+}
 
+
+    setInterval(() => {
+        getTempAsync()
+        getHumAsync()
+      }, 30000)
     
 })
+
